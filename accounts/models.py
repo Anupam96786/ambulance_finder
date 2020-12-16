82027fcf75012f7b1734d9e9afa57ffb1ac36f52
Anupam96786 <anupam96786@gmail.com>
2020-12-16 20:06:52 +0530

    model methods added

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class AccountType(models.Model):
     account_type = [
         ('user', 'User'),
-        ('ambulanceHub', 'Ambulance Hub'),
+        ('ambulance_hub', 'Ambulance Hub'),
         ('ambulance', 'Ambulance'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', blank=False, null=False, primary_key=True)
@@ -14,25 +14,41 @@ class AccountType(models.Model):
 
 class Users(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name='User', blank=False, null=False)
-    address = models.CharField('Address', max_length=100, blank=True, null=True)
+    address = models.CharField('Address', max_length=100, blank=True, null=True)    # user home address
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
+    def save(self, user: User, *args, **kwargs):
+        AccountType.objects.create(user=user, type='user')
+        super(Users, self).save(*args, **kwargs)
+        
+    def __str__():
+        return user.username
+
 
 class AmbulanceHub(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name='User', blank=False, null=False)
+    name = models.CharField(verbose_name='Hub Name', max_length=30, blank=False, null=False)    # Ambulance hub name to be displayed in user panel
     address = models.CharField('Address', max_length=100, blank=True, null=True)
     location = models.CharField('Location', max_length=50, blank=False, null=False)
 
     class Meta:
         verbose_name = 'Ambulance Hub'
         verbose_name_plural = 'Ambulance Hubs'
+    
+    def save(self, user: User, *args, **kwargs):
+        AccountType.objects.create(user=user, type='ambulance_hub')
+        super(AmbulanceHub, self).save(*args, **kwargs)
 
 
 class Ambulance(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name='User', blank=False, null=False)
-    ambulance_no = models.CharField('Ambulance Number', max_length=30, unique=True)
+    ambulance_no = models.CharField('Ambulance Number', max_length=30, unique=True, blank=False, null=False)
     hub = models.ForeignKey(AmbulanceHub, on_delete=models.CASCADE)
     available = models.BooleanField('Available')
+
+    def save(self, user: User, *args, **kwargs):
+        AccountType.objects.create(user=user, type='ambulance')
+        super(Ambulance, self).save(*args, **kwargs)
